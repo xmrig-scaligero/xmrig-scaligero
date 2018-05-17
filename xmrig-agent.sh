9b4f74b60e9e05e891232b582f6f09fe96f25c28
xmrig-scaligero <39375774+xmrig-scaligero@users.noreply.github.com>
2018-05-17 11:31:52 -0300

    Update xmrig-agent.sh

--- xmrig-agent.sh
+++ xmrig-agent.sh
@@ -1,9 +1,20 @@
-	#!/bin/sh
+#!/bin/sh
+
+# ScaligeroScript
+# Autor: XMRIG SCALIGERO
+# Version: 0-0-1a
+# XMRIG SCALIGERO AGENT: xmrig-agent.sh
+
+# SET SERVICE NAME 
 SERVICE='xmrig'
  
+# CHECK IF XMRIG SCALIGERO IS RUNNING 
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
+
+# IF XMRIG SCALIGERO IS ALREADY RUNNING DO NOTHING
 then
     echo "GOOD! $SERVICE IS RUNNING! EVERYTHING IS OK!"
+# ELSE RESTART XMRIG AGENT     
 else
     echo "BAD NEWS! $SERVICE IS NOT RUNNING. RESTARTING MAIN DAEMON PROCEDURE"
     cd /xmrig-scaligero
